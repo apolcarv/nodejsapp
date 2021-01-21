29c7b1779a34f3ea92d4a1827d54349731b7660a
apolcarv <69157339+apolcarv@users.noreply.github.com>
2021-01-21 16:43:45 -0500

    Create nodejsDSL.groovy

---
+++ nodejsDSL.groovy
@@ -0,0 +1,21 @@
+job('Aplicacion Node.js DSL') {
+    description('Aplicación Node JS DSL para el curso de Jenkins')
+    scm {
+        git('https://github.com/macloujulian/nodejsapp.git', 'master') { node ->
+            node / gitConfigName('macloujulian')
+            node / gitConfigEmail('macloujulian@gmail.com')
+        }
+    }
+    triggers {
+        scm('H/7 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs')
+    }
+    steps {
+        shell("npm install")
+    }
+    publishers {
+      mailer('alejandropolocarvajal@gmail.com', true, true)
+  }  
+}
